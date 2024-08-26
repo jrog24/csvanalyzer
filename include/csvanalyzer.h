d399b72f552cea40893a0d087ce87f18d472f6c5
Jonatan Rogozinski <jonatan.rogozinski@gmail.com>
2024-08-26 19:51:54 +0200

    Deleted some not needed comments

--- include/csvanalyzer.h
+++ include/csvanalyzer.h
@@ -3,15 +3,12 @@
 
 #include <time.h>
 
-// Maksymalne wartości
 #define MAX_LINE_LENGTH 10000
 #define MAX_ROWS 1000000
 #define MAX_FILE_SIZE 1073741824 // 1 GB
 
-// Funkcja do analizy pliku CSV
 void analyze_csv(const char *file_path);
 
-// Funkcja do obliczania czasu przetwarzania
 double processing_time(clock_t start, clock_t end);
 
-#endif // CSVANALYZER_H
+#endif
